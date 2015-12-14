b09d24f8409b51590cce92edf87c10309391973e
Bernardo Rittmeyer <bernardo@rittme.com>
2015-12-14 14:55:06 +0100

    clearer actions definition

--- Room.hs
+++ Room.hs
@@ -1,12 +1,27 @@
+module Room where
+
 import Data.String
+import Characters
 
 data Room = Room { description :: String, action :: Action }
   deriving (Show)
 
-data Action = Combat | Treasure | Damage Int | Choices { opt :: [String]}
+data Item = Weapon Object | Life Int
+  deriving (Show)
+
+data Action = Combat Character | Treasure Item | Damage Int | Choices { opt :: [String]}
   deriving (Show)
 
 rooms :: [Room]
 rooms = [
   Room "You wake up in the dungeon cell you have been rotting for ages. Everything sounds suspiciously quiet. You notice the celldoor is open." (Choices ["Go out"]),
-  Room "You walk into a room. As soon as you step in you feel the air is loaded with a toxic gas, you try to go out as soon as possible but get lightly poisoned." (Damage 10)]
+  Room "You walk into a room. As soon as you step in you feel the air is loaded with a toxic gas, you try to go out as soon as possible but get lightly poisoned." (Damage 10),
+  Room "You enter into a strange room. It's completely different from the rest of the dungeon, contains a magnificent font with healing water." (Treasure (Life 20)),
+  Room "You enter into a dark room. In a corner you see a closed chest. Opening it you see a Shock Scroll." (Treasure (Weapon (Object "Shock Scroll" 15)))
+  ]
+
+--showRoom :: Room -> IO()
+--showRoom (Room d a) = do
+--                        putStrLn d
+--                        putStrLn actionChoces
+--                        choice <- getLine
