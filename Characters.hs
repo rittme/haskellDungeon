eae137ecbfb70d208e3b25a4d751bd4c00d3f61b
modou6c <contact.modoucisse@gmail.com>
2015-12-15 20:23:45 +0100

    fighting !!!

--- Characters.hs
+++ Characters.hs
@@ -4,7 +4,6 @@ import Data.List
 import Data.Maybe
 import Data.Char
 
-
 data Object = Object {name :: String, damage :: Int}
   deriving(Show)
 
@@ -31,71 +30,83 @@ monsters = [Monster "Dragon" 100 (Object "Fire breath" 30),
             Monster "Ghost" 40 (Object "Soul touch" 15)]
 
 nameChar :: Character -> String
-nameChar (Monster s _ _) 	= s
-nameChar _ 					= "Player"
+nameChar (Monster s _ _)  = s
+nameChar _                = "Player"
 
 
 -- Player used Dagger: 10 damage
 -- Dragon used Fire breath: 30 damage
-
+-------------------------------------------------------------------------
+{-
+  ###### FIGHT #######
+-}
 zipBag :: [Object] -> [(Int, Object)] 
-zipBag list 	= zip [1..] list
+zipBag = zip [1..]
 
 printBag :: [(Int, Object)] -> IO ()
-printBag [] 		= putStrLn "*** No weapon in your bag \n 1) Run " 
-printBag (list) 	= mapM_ putStrLn [itemToString x | x <- list]
+printBag []   = putStrLn "*** No weapon in your bag \n 1) Run " 
+printBag list = mapM_ putStrLn [itemToString x | x <- list]
 
 itemToString :: (Int, Object) -> String
-itemToString i = show (fst i) ++ ") Attack with " ++ (name (snd i))
+itemToString i = show (fst i) ++ ") Attack with " ++ name (snd i)
 
 
-isIntValue :: [Char] -> Bool
-isIntValue value = length value > 0 && all isDigit value
+isIntValue :: String -> Bool
+isIntValue value = not (null value) && all isDigit value
 
 getObject :: String -> [(Int, Object)] -> Maybe (Int, Object)
-getObject _ [] 						= Nothing
+getObject _ []                      = Nothing
 getObject chx bag | isIntValue chx  = do
-										let x = read chx :: Int
-										object <- find (\t -> fst t == x) bag
-										return object
-				  				| otherwise 		= Nothing 
-
--- the player win the fight
-win :: Character -> Character -> Bool
-win (Player endurance _) (Monster _ life _) = endurance > 0 && life <= 0
-
--- the player lost the fight (died)
-lost :: Character -> Bool
-lost (Player endurance _) = endurance <= 0
+                                        let x = read chx :: Int
+                                        find (\ t -> fst t == x) bag
+                  | otherwise       = Nothing 
+-- The fight is end : The player or the monster is dead
+isEndfight :: Character -> Character -> Bool
+isEndfight (Player endur _) (Monster _ life _) = endur <= 0 || life <= 0
 
 
+getChoice :: [Object] -> IO String
+getChoice bag = do
+                  putStrLn "*** Please choose a weapon ***" 
+                  printBag (zipBag bag) 
+                  choice <- getLine
+                  if isIntValue choice
+                    then return choice
+                  else getChoice bag
 
-{- fight :: Character -> Character -> IO ()
-fight (Player l b) monster | win (Player l b) monster = putStrLn "you win !" -- to improve
-													 | lost (Player l b)				= putStrLn "you lost !" 
-													 | otherwise = do 
-																	printBag (zipBag b)
-																	choice <- getLine
-																	
-																	fight (battle choice b (Player l b)) (battle choice b monster)
-																	
+
+getFightStatus :: Character -> Object -> Character -> [String]
+getFightStatus (Player l b) (Object pn pdm) (Monster s life (Object mn mdm)) = 
+                ["---------------------------------- \n"
+                            ,"Player used " ++ pn ++ ": " ++ show pdm ++ " damage \n"
+                            ,s ++ " used " ++ mn ++ ": " ++ show mdm ++ " damage \n"
+                            ,"\nPlayer remaining life: " ++ show l ++ " \n"
+                            ,s ++ " remaining life: " ++ show life ++ " \n"
+                            ,"---------------------------------- \n"]
+
+
+showStatus :: Character -> Maybe (Int, Object) -> Character -> IO ()
+showStatus p o m  | isNothing o  = return ()
+                  | otherwise    = mapM_ putStrLn $ 
+                          getFightStatus p (snd (fromJust o)) m 
+
 
 -- TODO : check player's (and monster's ??) life and where
 fightRound :: Character -> Maybe (Int, Object) -> Character
-fightRound char obj | isNothing (obj) = char
-										| otherwise 	  	= attack (snd obj) char
+fightRound char obj | isNothing obj = char
+                    | otherwise     = attack (snd (fromJust obj)) char 
 
-battle:: String -> [Object] -> Character -> Character
-battle choice bag (Player l b) = do 
-																		zippedBag <- zipBag bag
-																		ob <- getObject choice zippedBag
-																		p  <- fightRound (Player l b) ob
-																		return p
+fight :: Character -> Character -> IO Character
+fight (Player l b) monster | isEndfight (Player l b) monster = return (Player l b)
+                           | otherwise = do 
+                                  choice <- getChoice b
+                                  let zippedBag = zipBag b
+                                  let ob  = getObject choice zippedBag
+                                  let p   = fightRound (Player l b) ob
+                                  let m   = fightRound monster ob
+                                  showStatus p ob m
+                                  fight p m
+                                  
 
-battle choice bag (Monster s life d) = do 
-																		zippedBag <- zipBag bag
-																		ob <- getObject choice zippedBag
-																		m  <- fightRound (Monster s life d) ob
-																		return m -}
 
-
+-------------------------------------------------------------------------
