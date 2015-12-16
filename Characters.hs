23d1d77d3a4cf5daa6fb43a9929955014040b45a
Bernardo Rittmeyer <bernardo@rittme.com>
2015-12-16 13:02:53 +0100

    Trailling spaces cleanup and new monsterObject function

--- Characters.hs
+++ Characters.hs
@@ -34,8 +34,6 @@ attack (Object _ damage) (Player life bag) =  do
 testbag :: [Object]
 testbag = [Object "Dagger" 10, Object "Shock scroll" 15, Object "Dart" 5]
 
-player = Player 100 testbag
-
 monsters :: [Character]
 
 monsters = [Monster "Dragon" 100 (Object "Fire breath" 30),
@@ -46,17 +44,18 @@ nameChar :: Character -> String
 nameChar (Monster s _ _)  = s
 nameChar _                = "Player"
 
--- Player used Dagger: 10 damage
--- Dragon used Fire breath: 30 damage
+monsterObject :: Character -> Object
+monsterObject (Monster _ _ o) = o
+
 -------------------------------------------------------------------------
 {-
   ###### FIGHT #######
 -}
-zipBag :: [Object] -> [(Int, Object)] 
+zipBag :: [Object] -> [(Int, Object)]
 zipBag = zip [1..]
 
 printBag :: [(Int, Object)] -> IO ()
-printBag []   = putStrLn "*** No weapon in your bag \n 1) Run " 
+printBag []   = putStrLn "*** No weapon in your bag \n 1) Run "
 printBag list = mapM_ putStrLn [itemToString x | x <- list]
 
 itemToString :: (Int, Object) -> String
@@ -71,7 +70,7 @@ getObject _ []                      = Nothing
 getObject chx bag | isIntValue chx  = do
                                         let x = read chx :: Int
                                         find (\ t -> fst t == x) bag
-                  | otherwise       = Nothing 
+                  | otherwise       = Nothing
 -- The fight is end : The player or the monster is dead
 isEndfight :: Character -> Character -> Bool
 isEndfight (Player endur _) (Monster _ life _) = endur <= 0 || life <= 0
@@ -79,8 +78,8 @@ isEndfight (Player endur _) (Monster _ life _) = endur <= 0 || life <= 0
 
 getChoice :: [Object] -> IO String
 getChoice bag = do
-                  putStrLn "*** Please choose a weapon ***" 
-                  printBag (zipBag bag) 
+                  putStrLn "*** Please choose a weapon ***"
+                  printBag (zipBag bag)
                   choice <- getLine
                   if isIntValue choice
                     then return choice
@@ -88,7 +87,7 @@ getChoice bag = do
 
 
 getFightStatus :: Character -> Object -> Character -> [String]
-getFightStatus (Player l b) (Object pn pdm) (Monster s life (Object mn mdm)) = 
+getFightStatus (Player l b) (Object pn pdm) (Monster s life (Object mn mdm)) =
                 ["---------------------------------- \n"
                             ,"Player used " ++ pn ++ ": " ++ show pdm ++ " damage \n"
                             ,s ++ " used " ++ mn ++ ": " ++ show mdm ++ " damage \n"
@@ -99,26 +98,25 @@ getFightStatus (Player l b) (Object pn pdm) (Monster s life (Object mn mdm)) =
 
 showStatus :: Character -> Maybe (Int, Object) -> Character -> IO ()
 showStatus p o m  | isNothing o  = return ()
-                  | otherwise    = mapM_ putStrLn $ 
-                          getFightStatus p (snd (fromJust o)) m 
+                  | otherwise    = mapM_ putStrLn $
+                          getFightStatus p (snd (fromJust o)) m
 
 
 -- TODO : check player's (and monster's ??) life and where
 fightRound :: Character -> Maybe (Int, Object) -> IO Character
 fightRound char obj | isNothing obj = return char
-                    | otherwise     = attack (snd (fromJust obj)) char 
+                    | otherwise     = attack (snd (fromJust obj)) char
 
 fight :: Character -> Character -> IO Character
 fight (Player l b) monster | isEndfight (Player l b) monster = return (Player l b)
-                           | otherwise = do 
+                           | otherwise = do
                                   choice <- getChoice b
                                   let zippedBag = zipBag b
                                   let ob  = getObject choice zippedBag
                                   p <- fightRound (Player l b) ob
-                                  m <- fightRound monster ob
+                                  m <- fightRound monster (monsterObject monster)
                                   showStatus p ob m
                                   fight p m
-                                  
 
 
 -------------------------------------------------------------------------
