d5b6dd81aa2dd29bc0f1ff6c60bcc32bf965fbb1
Bernardo Rittmeyer <bernardo@rittme.com>
2015-12-16 20:06:44 +0100

    Helper comments

--- Helpers.hs
+++ Helpers.hs
@@ -3,9 +3,11 @@ module Helpers where
 import Data.Maybe
 import Data.Char
 
+-- Read a numeric and return a Just or Nothing if not numeric
 maybeRead :: (Read a, Num a) => String -> Maybe a
 maybeRead = fmap fst . listToMaybe . filter (all isSpace . snd) . reads
 
+-- Prompt the user for an Int between gte and lte
 promptInt :: (Read b, Num b) => String -> Int -> Int -> IO b
 promptInt s gte lte = do
                         putStr s
