c082a38c679ba7e61fb83483f5cd1b0749e012e9
Bernardo Rittmeyer <bernardo@rittme.com>
2015-12-16 02:14:21 +0100

    Main game loop functions

--- Main.hs
+++ Main.hs
@@ -1,3 +1,9 @@
 import Characters
 import Room
 import GameIO
+
+startGame :: IO()
+startGame = opening >> gameLoop player
+
+gameLoop :: Character -> IO b
+gameLoop c = randomRoom >>= \r -> playRoom r c >>= \p -> gameLoop p
