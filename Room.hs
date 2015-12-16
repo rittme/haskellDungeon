5f18d7ffbdf1d6c1d2ae45638d3ed937554e9937
Bernardo Rittmeyer <bernardo@rittme.com>
2015-12-16 15:39:04 +0100

    Room comments and some cleanup

--- Room.hs
+++ Room.hs
@@ -6,15 +6,24 @@ import Test.QuickCheck
 import Helpers
 import Characters
 
+-- Room type definition, contains the room description and it's action
 data Room = Room { description :: String, action :: Action }
   deriving (Show)
 
+-- Definition of the types of Treasure one can find: weapons, healing or empty
 data Item = Weapon Object | Healing Int | Empty
   deriving (Show)
 
+{- Posible actions definition
+  Combat:   a combat action that takes the enemy as it's character
+  Treasure: a treasure to be collected
+  Damage:   damage to be taken by the character
+  Choices:  an array of choices the character can make
+-}
 data Action = Combat Character | Treasure Item | Damage Int | Choices [String]
   deriving (Show)
 
+-- Display the different choices the user have for each action
 actionChoices :: Action -> String
 actionChoices (Treasure Empty) = "The chest was empty.\n1) Continue\n"
 actionChoices (Treasure (Weapon o)) = "You find a " ++ name o ++
@@ -26,6 +35,7 @@ actionChoices (Damage d) = "1) Take " ++ show d ++ " damage and leave the room.\
 actionChoices (Choices opts) = unlines $ map (\(x,y) -> show x ++ ") " ++ y)(zip [1..] opts)
 actionChoices (Combat m) = "1) Fight " ++ nameChar m
 
+-- Returns the character affected by the changes of the chosen action
 actionEffect :: Int -> Action -> Character -> Character
 actionEffect _ (Treasure Empty) p = p
 actionEffect 1 (Treasure (Weapon o))  (Player life bag) = Player life (o:bag)
@@ -36,12 +46,12 @@ actionEffect _ (Damage d)             (Player life bag) = Player (life - d) bag 
 actionEffect _ (Choices _)            player            = player
 actionEffect _ (Combat m)             (Player life bag) = error "Combat should never get here!"
 
-showRoom :: Room -> IO()
-showRoom (Room d a) = putStrLn d
+-------------------- IO --------------------
 
-runChoice :: Action -> Character -> IO Character
-runChoice (Combat m) p = fight p m
-runChoice a p = promptInt (actionChoices a) 1 (actionLgt a) >>= \c ->
+-- Prompts the user for the desired choice of action
+makeChoice :: Action -> Character -> IO Character
+makeChoice (Combat m) p = fight p m
+makeChoice a p = promptInt (actionChoices a) 1 (actionLgt a) >>= \c ->
                 return (actionEffect c a p)
   where actionLgt :: Action -> Int
         actionLgt (Treasure Empty) = 1
@@ -49,32 +59,18 @@ runChoice a p = promptInt (actionChoices a) 1 (actionLgt a) >>= \c ->
         actionLgt (Damage _)       = 1
         actionLgt (Choices n)      = length n
 
-randomRoom :: IO Room
-randomRoom = (generate . elements) rooms
-
+-- Plays the passed room with the passed character
 playRoom :: Room -> Character -> IO Character
-playRoom r c = showRoom r >> runChoice (action r) c
+playRoom r c = putStrLn (description r) >> makeChoice (action r) c
 
-rooms :: [Room]
-rooms = [
-  Room "You wake up in the dungeon cell you have been rotting for ages. Everything sounds suspiciously quiet. You notice the celldoor is open." (Choices ["Go out"]),
-  Room "You walk a room. As soon as you step in you feel the air is loaded with a toxic gas, you try to go out as soon as possible but get lightly poisoned." (Damage 10),
-  Room "You enter a strange room. It's completely different from the rest of the dungeon, contains a magnificent font with healing water." (Treasure (Healing 20)),
-  Room "You enter a dark room. In a corner you see a closed chest. Opening it you see a Shock Scroll." (Treasure (Weapon (Object "Sword" 25))),
-  Room "You enter a large hall, filled with gold and gems. Standing over it there is a mighty dragon. He looks at you with an greedy look and jumps straight to combat.\n\
-\ <>=======()\n\
-\(/\\___   /|\\\\          ()==========<>_\n\
-\      \\_/ | \\\\        //|\\   ______/ \\)\n\
-\        \\_|  \\\\      // | \\_/\n\
-\          \\|\\/|\\_   //  /\\/\n\
-\           (oo)\\ \\_//  /\n\
-\          //_/\\_\\/ /  |\n\
-\         @@/  |=\\  \\  |\n\
-\              \\_=\\_ \\ |\n\
-\                \\==\\ \\|\\_\n\
-\             __(\\===\\(  )\\\n\
-\            (((~) __(_/   |\n\
-\                 (((~) \\  /\n\
-\                 ______/ /\n\
-\                 '------'" (Combat (Monster "Dragon" 100 (Object "Fire breath" 3)))
-  ]
+
+-- Generates a random room
+--randomRoom :: IO Room
+--randomRoom = (generate . elements) dungeon
+--
+--roomTestDescriptions :: [String]
+--roomTestDescriptions = ["A small room.", "A bright room.",
+--                    "A big room.", "A dark room."]
+--roomTestActions :: [Action]
+--roomTestActions = []
+--
